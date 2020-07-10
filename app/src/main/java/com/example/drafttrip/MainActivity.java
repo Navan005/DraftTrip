5b4b2ad3155da96f31c4386b01ab02ae28e7545e
Navan005 <navandeep0511@gmail.com>
2020-07-10 00:13:34 -0400

    Commit 1: List Activity  (Navandeep)

--- app/src/main/java/com/example/drafttrip/MainActivity.java
+++ app/src/main/java/com/example/drafttrip/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.drafttrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button nextpagebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextpagebtn=(Button) findViewById(R.id.mainBtn);
+
+        nextpagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
