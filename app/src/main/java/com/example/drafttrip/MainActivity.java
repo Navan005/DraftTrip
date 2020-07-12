dfc997ee42c90298ef9d398ad7e7750f02f21fb1
Navan005 <navandeep0511@gmail.com>
2020-07-11 21:37:29 -0400

    Commit: Settings Activity

--- app/src/main/java/com/example/drafttrip/MainActivity.java
+++ app/src/main/java/com/example/drafttrip/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -43,6 +45,27 @@ signup=(Button)findViewById(R.id.button2);
 
 
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent=new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
     
 }
---
+++ app/src/main/java/com/example/drafttrip/SettingsActivity.java
@@ -0,0 +1,71 @@
+package com.example.drafttrip;
+
+import android.os.Bundle;
+import android.preference.SwitchPreference;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public SwitchPreference testPref;
+    public static final String switch_preferences = "switch_preference";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
+
+            EditTextPreference signaturePreference = findPreference("signature");
+            if (signaturePreference != null) {
+                Toast.makeText(getActivity().getApplicationContext(),"Signature already set.",Toast.LENGTH_SHORT).show();
+            }
+
+
+            final androidx.preference.SwitchPreference onOffRandomColor = (androidx.preference.SwitchPreference) findPreference(SettingsActivity.switch_preferences);
+            onOffRandomColor.setOnPreferenceChangeListener(new androidx.preference.SwitchPreference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    if(onOffRandomColor.isChecked()){
+                        Toast.makeText(getActivity().getApplicationContext(),"Notifications are OFF",Toast.LENGTH_SHORT).show();
+
+                        // Checked the switch programmatically
+                        onOffRandomColor.setChecked(false);
+                    }else {
+                        Toast.makeText(getActivity().getApplicationContext(),"Notifications are ON",Toast.LENGTH_SHORT).show();
+
+                        // Unchecked the switch programmatically
+                        onOffRandomColor.setChecked(true);
+                    }
+                    return false;
+
+                }
+            });
+
+
+
+        }
+    }
+}
